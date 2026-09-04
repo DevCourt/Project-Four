4e8580ff38d0fb4c0b414c847d37db236097becb
Court <devarctic107@gmail.com>
2026-09-04 10:45:02 -0400

    Update UltimateBattleApp.java

--- UltimateBattleApp.java
+++ UltimateBattleApp.java
@@ -1,9 +1,9 @@
 public class UltimateBattleApp {
 	public static void main(String[] args) {
 		Water water = new Water();
-		Contender contender1 = new Contender("Micheal", 1000, 2000, water);
-		Contender contender2 = new Contender("Trevor", 1500, 3000, water);
-		Contender contender3 = new Contender("Franklin", 2000, 4000, water);
+		Contender contender1 = new Contender("Steve", 1000, 2000, water);
+		Contender contender2 = new Contender("Alex", 1500, 3000, water);
+		Contender contender3 = new Contender("Bob", 2000, 4000, water);
 
 		Thread thread1 = new Thread(contender1);
 		Thread thread2 = new Thread(contender2);
